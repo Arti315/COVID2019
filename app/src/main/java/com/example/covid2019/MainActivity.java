84914a19f8eddb1c62b8519c69d7584628448c02
pramodkumar11 <pramodkumarp6@gmail.com>
2020-04-27 06:20:03 +0530

    welcome

---
+++ app/src/main/java/com/example/covid2019/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.covid2019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
